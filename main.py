c9e58b19d309f367d5b8cc81b7baf6cb132726b1
root <root@gd.asia-east1-b.c.elated-terminal-322508.internal>
2022-02-11 08:47:44 +0000

    label

--- main.py
+++ main.py
@@ -276,6 +276,7 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     
     
 def save_issue(issue, me, dir_name=BACKUP_DIR):
+    labels = get_repo_labels(repo)
     md_name = os.path.join(
         dir_name, f"{issue.number}_{issue.title.replace(' ', '.')}.md"
     )
