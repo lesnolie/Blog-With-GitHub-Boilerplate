06a6691da5d0ac0c05cc534c0dc10e5c516978cc
root <root@gd.asia-east1-b.c.elated-terminal-322508.internal>
2022-02-11 08:04:45 +0000

    edit

--- main.py
+++ main.py
@@ -280,21 +280,22 @@ def save_issue(issue, me, dir_name=BACKUP_DIR):
         dir_name, f"{issue.number}_{issue.title.replace(' ', '.')}.md"
     )
     with open(md_name, "w") as f:
-        f.write("
-        --- +\n +
-        layout: post+\n +
-        title: + {issue.title}+\n +
-        slug: +\n +
-        date: + {time} +\n +
-        status: publish+\n +
-        author: Leslie+\n +
-        categories: +\n +
-          -+ {label.name} +\n +
-        tags:+\n +
-          - +\n +
-          - +\n 
-        excerpt: +\n +
-        --- + \n")
+        f.write(
+        "---" +\n +
+        "layout: post"+\n +
+        "title:" + {issue.title}+\n +
+        "slug:" +\n +
+        "date:" + {time} +\n +
+        "status: publish"+\n +
+        "author: Leslie"+\n +
+        "categories:" +\n +
+          "-"+ {label.name} +\n +
+        "tags:"+\n +
+          "-" +\n +
+          "-"+\n 
+        "excerpt:" +\n +
+        "---" + \n
+        )
        
         f.write(f"# [{issue.title}]({issue.html_url})\n\n")
         f.write(issue.body)
