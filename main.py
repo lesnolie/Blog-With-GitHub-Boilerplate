e05d5e8d210f824cd8d84e0eb13ed0a58b190782
Leslie <81410185+lesnolie@users.noreply.github.com>
2022-04-28 19:12:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ My personal blog using issues & GitHub Actions and Maverick .
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
 
-BACKUP_DIR = "BACKUP"
+BACKUP_DIR = "src"
 ANCHOR_NUMBER = 5
 TOP_ISSUES_LABELS = ["Top"]
 TODO_ISSUES_LABELS = ["TODO"]
