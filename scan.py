a1554b31078067a05d0ebac5052bf610eb1410d8
Leslie <81410185+lesnolie@users.noreply.github.com>
2023-02-24 11:25:12 +0800

    Delete scan.py

--- scan.py
+++
@@ -1,9 +0,0 @@
-from notion_client import Client
-import os
-
-notion = Client(auth=os.environ["NOTION_API_KEY"])
-db = notion.databases.query(database_id=os.environ["NOTION_DATABASE_ID"]).get("results")
-
-# do something with the query results, such as:
-for result in db:
-  print(result)
